e420ec03b68b419c4b5cda4ff19f612751bf9ae7
ewfreesRes <ewfrees@wisc.edu>
2020-09-30 13:30:58 -0500

    jExcel Pagination a problem, so removed it

--- tab.js
+++ tab.js
@@ -59,6 +59,13 @@ $(document).ready(function(){
 
 <script src=https://cdn.datacamp.com/datacamp-light-latest.min.js></script>
 
+<script src="https://bossanova.uk/jexcel/v4/jexcel.js"></script>
+<link rel="stylesheet" href="https://bossanova.uk/jexcel/v4/jexcel.css" type="text/css" />
+<script src="https://bossanova.uk/jsuites/v3/jsuites.js"></script>
+<link rel="stylesheet" href="https://bossanova.uk/jsuites/v3/jsuites.css" type="text/css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+
 <script>
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover(); 
